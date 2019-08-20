0b6bf1393f94bb474dc049c4e97d53b05380aa9c
unknown <prakharkhandelwal11@gmail.com>
2019-08-20 11:17:21 +0530

    Login functionalities

--- src/main/java/com/resilience/spring/repository/CustomerRepository.java
+++ src/main/java/com/resilience/spring/repository/CustomerRepository.java
@@ -1,9 +1,26 @@
 package com.resilience.spring.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.resilience.spring.model.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
+	
+	public Customer findByEmail();
+//	public static String verifyLogin(String email, String password)
+//	{		
+//		Customer customer;
+//		String mail = customer.getEmail();
+//		String pass = customer.getPassword();
+//		
+//		if(mail == email && pass == password)
+//		{
+//			return "exist";
+//		}
+//		else 
+//			return "not exist";
+//	}
 }
